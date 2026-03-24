21464263e010edc72f8f9c0451bf0a21d74b7e4d
woo628 <duddn2500@gmail.com>
2026-03-24 17:16:58 +0900

    TestCar01 생성

---
+++ src/ex01/TestCalc01.java
@@ -0,0 +1,34 @@
+package ex01;
+
+public class TestCalc01 {
+
+	public static void main(String[] args) {
+
+		String name1 = "아빠차";
+		String color1 = "Black";
+		int year1 = 2023;
+		
+		System.out.println("차이름:" + name1 );
+		System.out.println("색깔:" + color1 );
+		System.out.println("연식:" + year1 );
+		System.out.println();
+		
+		String name2 = "엄마차";
+		String color2 = "Red";
+		int year2 = 2025;
+		
+		System.out.println("차이름:" + name2 );
+		System.out.println("색깔:" + color2 );
+		System.out.println("연식:" + year2 );
+		System.out.println();
+		
+		String name3 = "내차";
+		String color3 = "Pink";
+		int year3 = 2026;
+		
+		System.out.println("차이름:" + name3 );
+		System.out.println("색깔:" + color3 );
+		System.out.println("연식:" + year3 );
+	}
+
+}
